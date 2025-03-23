ea96063f027eeefd4a58e60b985ac448c70b2ef9
PresentKim <debe3721@gmail.com>
2025-03-23 21:51:49 +0900

    feat(layout): Enhance metadata for improved SEO and sharing
    - Added Open Graph and Twitter Card metadata for better social media integration.
    - Included site domain, name, description, and thumbnail for enhanced visibility.

---
+++ src/config/metadata.ts
@@ -0,0 +1,4 @@
+export const siteDomain = 'https://present.kim'
+export const siteName = '현재는개발중'
+export const siteDescription = '프로그래밍을 좋아하는 김현재의 개발 블로그'
+export const siteThumbnail = '/thumbnail.png'
