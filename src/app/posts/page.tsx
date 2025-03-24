b888bec9bf6e45ae0a97419dce670d935ea8c3ce
PresentKim <debe3721@gmail.com>
2025-03-24 11:12:36 +0900

    feat(home): Add metadata and update pages content
    - Introduced metadata for the page to enhance SEO.
    - Updated page content to indicate that it is under construction.
    - Added new pages for posts and projects with corresponding metadata.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,10 +1,11 @@
 import type {Metadata} from 'next'
 
 export const metadata: Metadata = {
-  description: '프로그래밍을 좋아하는 개발자 김현재의 포트폴리오',
+  title: '블로그',
+  description: '프로그래밍을 좋아하는 개발자 김현재의 포스트 목록',
 }
 
-export default function Home() {
+export default function Posts() {
   return (
     <>
       <h1 className="text-center text-4xl font-bold">공사중</h1>
