5a3ed2546d2c7a6570cd8778f9c80915e67e62a1
PresentKim <debe3721@gmail.com>
2025-03-27 08:01:14 +0900

    feat(sitemap): Add dynamic post links to sitemap generation
    - Implemented functionality to fetch and include post URLs in the sitemap.
    - Each post URL includes the last modified date and a priority value.

--- src/app/(resources)/(feed)/sitemap.ts
+++ src/app/(resources)/(feed)/sitemap.ts
@@ -1,4 +1,5 @@
 import {MetadataRoute} from 'next'
+import {getPostList} from '@/lib/content/posts'
 import {siteDomain} from '@/lib/metadata'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -6,7 +7,16 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: `${siteDomain}`,
       lastModified: new Date(),
+      priority: 1,
     },
   ]
+  const posts = await getPostList()
+  posts.forEach(post => {
+    links.push({
+      url: `${siteDomain}/posts/${post.slug}`,
+      lastModified: new Date(post.frontmatter.date),
+      priority: 0.8,
+    })
+  })
   return links
 }
