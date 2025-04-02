bf44f74d1fd0aa898d40de38543a25a29f86a121
PresentKim <debe3721@gmail.com>
2025-04-02 23:38:01 +0900

    feat(home): Display recent posts using PostCard component
    - Implemented functionality to fetch and display the six most recent posts.
    - Enhanced layout for better accessibility and user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,32 @@
 import type {Metadata} from 'next'
+import {PostCard} from '@/components/PostCard'
+import {getPostList} from '@/lib/content/posts'
+import {PostInfo} from '@/types/post'
 
 export const metadata: Metadata = {
   description: '프로그래밍을 좋아하는 개발자 김현재의 포트폴리오',
 }
 
-export default function Home() {
+export default async function Home() {
+  const allPostsData = await getPostList()
+  const recentPosts = allPostsData.slice(0, 6)
+
   return (
-    <>
-      <h1 className="text-center text-4xl font-bold">공사중</h1>
-    </>
+    <section aria-label="posts" className="container mx-auto max-w-4xl">
+      <h2 className="mb-4 ml-4 text-2xl font-semibold">최근 포스트</h2>
+      <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:grid-cols-3">
+        {recentPosts.map((post: PostInfo) => (
+          <PostCard
+            key={post.path}
+            category={post.category}
+            title={post.frontmatter.title}
+            description={post.frontmatter.description || ''}
+            date={post.frontmatter.date}
+            slug={post.path}
+            thumbnail={post.frontmatter.thumbnail}
+          />
+        ))}
+      </div>
+    </section>
   )
 }
